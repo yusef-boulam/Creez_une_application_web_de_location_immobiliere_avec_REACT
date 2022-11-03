67f13d28342693a5cf9ae603ceec550073fd6a3e
yusef <yusefdev@outlook.fr>
2022-11-03 22:08:47 +0100

    ajout de l'algo openClose sur la page APropos

--- src/Pages/APropos.jsx
+++ src/Pages/APropos.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import "../styles/APropos.css";
@@ -6,21 +6,33 @@ import pictureApropos from "../assets/Apropos/pictureAPropos.png";
 import Dropdown from "../components/Dropdown";
 
 export default function APropos() {
+  //state
+
+  const [arrayDropdown, setArrayDropdown] = useState([
+    { id: 1, title: "Fiabilité", Open: false},
+    { id: 2, title: "Respect", Open: false },
+    { id: 3, title: "Service", Open: false },
+    { id: 4, title: "Responsabilité", Open: false },
+  ]);
+
+  console.log(arrayDropdown)
+  // affichage
+
   return (
-    <div className="APropos" >
+    <div className="APropos">
       <header>
-        <Banner />  
-          <img
-            src={pictureApropos}
-            alt="montagnes"
-            className="picture-APropos-img"
-          />
+        <Banner />
+        <img
+          src={pictureApropos}
+          alt="montagnes"
+          className="picture-APropos-img"
+        />
       </header>
-      <Dropdown texte="Fiabilité"/>
-      <Dropdown texte="Respect"/>
-      <Dropdown texte="Service"/>
-      <Dropdown texte="Responsabilité"/>
+      {arrayDropdown.map((dropdown) => (
+        <Dropdown key={dropdown.id} texte={dropdown.title} arrayDropdown={arrayDropdown} setArrayDropdown= {setArrayDropdown} id ={dropdown.id} />
+      ))}
       <Footer />
+      Drop
     </div>
   );
 }
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,14 +2,39 @@ import "../styles/Dropdown.css";
 import React from "react";
 import Vector from "../assets/Apropos/Vector.png";
 
-function Dropdown({texte}) {
-  // state (état, données)
+function Dropdown({texte, arrayDropdown, setArrayDropdown, id}) {
+ 
+  const openClose = (id) => {
+ // copie du state 
+ const arrayDropdownModify = [...arrayDropdown]
+
+ // manipule la copie du state
+
+
+    
+   const dropdownClic = arrayDropdownModify.find(dropdown => id === dropdown.id)
+     if(dropdownClic.Open === true) {
+      arrayDropdownModify.find(dropdown => id === dropdown.id).Open = false
+
+  } else if (dropdownClic.Open === false){
+    arrayDropdownModify.find(dropdown => id === dropdown.id).Open = true
+   }
+   
+     // modifie le state
+  
+   setArrayDropdown(arrayDropdownModify)
+
+  }
+
+
+ 
 
   return (
     // affichage
     <div className="container-Dropdown">
         <h2>{`${texte}`}</h2>
       <img
+        onClick={()=> openClose(id)}
         src={Vector}
         alt="fleche ouverture descriptifs"
       />

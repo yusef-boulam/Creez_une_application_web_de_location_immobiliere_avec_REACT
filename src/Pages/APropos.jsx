dbc0ec78664197e0553e7f59e75e3e6761a5d83e
yusef <yusefdev@outlook.fr>
2022-11-04 20:00:00 +0100

    composant dropdown termine + page APropos termine

--- src/Pages/APropos.jsx
+++ src/Pages/APropos.jsx
@@ -9,10 +9,10 @@ export default function APropos() {
   //state
 
   const [arrayDropdown, setArrayDropdown] = useState([
-    { id: 1, title: "Fiabilité", Open: false},
-    { id: 2, title: "Respect", Open: false },
-    { id: 3, title: "Service", Open: false },
-    { id: 4, title: "Responsabilité", Open: false },
+    { id: 1, title: "Fiabilité", Open: false, description: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." },
+    { id: 2, title: "Respect", Open: false, description: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."},
+    { id: 3, title: "Service", Open: false, description: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." },
+    { id: 4, title: "Responsabilité", Open: false, description: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." },
   ]);
 
   console.log(arrayDropdown)
@@ -29,7 +29,7 @@ export default function APropos() {
         />
       </header>
       {arrayDropdown.map((dropdown) => (
-        <Dropdown key={dropdown.id} texte={dropdown.title} arrayDropdown={arrayDropdown} setArrayDropdown= {setArrayDropdown} id ={dropdown.id} />
+        <Dropdown key={dropdown.id} texte={dropdown.title} arrayDropdown={arrayDropdown} setArrayDropdown= {setArrayDropdown} id ={dropdown.id}/>
       ))}
       <Footer />
       Drop
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -5,13 +5,11 @@ import Vector from "../assets/Apropos/Vector.png";
 function Dropdown({texte, arrayDropdown, setArrayDropdown, id}) {
  
   const openClose = (id) => {
+
  // copie du state 
  const arrayDropdownModify = [...arrayDropdown]
 
  // manipule la copie du state
-
-
-    
    const dropdownClic = arrayDropdownModify.find(dropdown => id === dropdown.id)
      if(dropdownClic.Open === true) {
       arrayDropdownModify.find(dropdown => id === dropdown.id).Open = false
@@ -21,16 +19,16 @@ function Dropdown({texte, arrayDropdown, setArrayDropdown, id}) {
    }
    
      // modifie le state
-  
    setArrayDropdown(arrayDropdownModify)
 
   }
 
+  // AFFICHAGE
+  const dropdownClic = arrayDropdown.find(dropdown => id === dropdown.id)
 
- 
-
+if (dropdownClic.Open === true){
+// si descrpitif ouvert
   return (
-    // affichage
     <div className="container-Dropdown">
         <h2>{`${texte}`}</h2>
       <img
@@ -42,4 +40,22 @@ function Dropdown({texte, arrayDropdown, setArrayDropdown, id}) {
   );
 }
 
+else {
+  // si descrpitif ferme
+  return (
+    <div className="gloabal-container">
+    <div className="container-Dropdown">
+        <h2>{`${texte}`}</h2>
+      <img className="img-close"
+        onClick={()=> openClose(id)}
+        src={Vector}
+        alt="fleche ouverture descriptifs"
+      />
+    </div>
+    <div className="descriptif"> {dropdownClic.description}</div>
+    </div>
+  );
+
+}
+}
 export default Dropdown;

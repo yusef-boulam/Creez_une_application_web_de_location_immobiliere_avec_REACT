03e211269752b1b23096fc6dfeede24d8ab6ae25
yusef <yusefdev@outlook.fr>
2022-11-19 20:16:40 +0100

    reglage css slideshow

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,22 +2,16 @@ import { Link } from "react-router-dom";
 import React from "react";
 import "../styles/Card.css";
 
-
-function Card({id, cover, title}) {
-
+function Card({ id, cover, title }) {
   return (
- 
-        <Link
-          to={`/ficheLogement/${id}`}
-          className="card"
-        >
-          <div className="imgCard">
-          <img src={cover} alt="appartement" />
-          <div className="linearGradient"></div>
-          </div>
-          <h2>{title}</h2>
-       
-        </Link>
-)}
+    <Link to={`/ficheLogement/${id}`} className="card">
+      <div className="imgCard">
+        <img src={cover} alt="appartement" />
+        <div className="linearGradient"></div>
+      </div>
+      <h2>{title}</h2>
+    </Link>
+  );
+}
 
 export default Card;
--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -14,9 +14,9 @@ function Slideshow(data) {
     //on recupere l'index de la photo precedente dans le tableau pictures
     let newIndex = data.data.pictures.indexOf(picture) - 1;
     // si le nouvel index est negatif on repart avec l index le plus grand du tableau
-    
-      newIndex < 0 && (newIndex = data.data.pictures.length - 1);
-    
+
+    newIndex < 0 && (newIndex = data.data.pictures.length - 1);
+
     //on sauvegarde dans le STATE
     setPictures(data.data.pictures[newIndex]);
   }
@@ -27,21 +27,19 @@ function Slideshow(data) {
     //on recupere l'index de la photo suivante dans le tableau pictures
     let newIndex = data.data.pictures.indexOf(picture) + 1;
     // si le nouvel index est superieur à la taille du tableau on repart au debut du tableau
-    
-      newIndex === data.data.pictures.length && (newIndex = 0);
-    
+
+    newIndex === data.data.pictures.length && (newIndex = 0);
+
     //on sauvegarde dans le STATE
     setPictures(data.data.pictures[newIndex]);
   }
-  
 
-//FONCTION BALL
-//on recupere l'id de la ball qui contient l'url de l'image et on met à jours le state.
+  //FONCTION BALL
+  //on recupere l'id de la ball qui contient l'url de l'image et on met à jours le state.
   function handleClickBall(event) {
     event.preventDefault();
-      //on sauvegarde dans le STATE
-    setPictures(event.target.id)
-
+    //on sauvegarde dans le STATE
+    setPictures(event.target.id);
   }
 
   return (
@@ -50,37 +48,37 @@ function Slideshow(data) {
 
       {/* affichage des fleches et des balles uniquement si plusieurs photos */}
       {data.data.pictures.length > 1 && (
-        <div className="arrow">
+        <div className="arrowBall">
+          <div className="container-arrow">
+            {/* arrow back */}
+            <img
+              className="arrow_back"
+              onClick={handleClickBack}
+              src={arrowBack}
+              alt="arrow back"
+            />
 
-          
-          {/* arrow back */}
-          <img
-            className="arrow_back"
-            onClick={handleClickBack}
-            src={arrowBack}
-            alt="arrow back"
-          />
+            {/* arrow forward */}
+            <img
+              className="arrow_forward"
+              onClick={handleClickForward}
+              src={arrowForward}
+              alt="arrow forward"
+            />
+          </div>
 
           {/* BALL */}
-      
-          <div className="container-ball">
-        {data.data.pictures.map((picture) => (
-          <div className="ball"
-          id= {picture}
-          key={picture}
-          onClick={handleClickBall}
 
-          ></div>
-        ))}
-      </div>
-      
-          {/* arrow forward */}
-          <img
-            className="arrow_forward"
-            onClick={handleClickForward}
-            src={arrowForward}
-            alt="arrow forward"
-          />
+          <div className="container-ball">
+            {data.data.pictures.map((picture) => (
+              <div
+                className="ball"
+                id={picture}
+                key={picture}
+                onClick={handleClickBall}
+              ></div>
+            ))}
+          </div>
         </div>
       )}
     </div>

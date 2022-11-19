f16eec889fc5432b739d5432e52d6543276e94a7
yusef <yusefdev@outlook.fr>
2022-11-19 17:00:31 +0100

    reglage CSS card homepage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,8 +11,12 @@ function Card({id, cover, title}) {
           to={`/ficheLogement/${id}`}
           className="card"
         >
+          <div className="imgCard">
           <img src={cover} alt="appartement" />
+          <div className="linearGradient"></div>
+          </div>
           <h2>{title}</h2>
+       
         </Link>
 )}
 
